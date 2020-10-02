0e21cb0725dd1ab84200763ac8b4b76ba977bd32
nicholas ray <nicholas@hoare7.cs.umsl.edu>
2020-10-02 08:43:51 -0500

    :sparkles: Implement function to check if a string is a palindrome

--- palin.c
+++ palin.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+void is_palindrome(char str[]);
 
 int main()
 {
-  printf("Hello World!\n");
+  char str[] = "racecar";
+  char str2[] = "nope";
+  is_palindrome(str);  
+  is_palindrome(str2);
   return 0;
+}
+
+void is_palindrome(char str[])
+{
+  int i = 0; // Start of string index
+  int j = strlen(str) - 1; // End of string index
+
+  while (j > 1)
+  {
+    if (str[i++] != str[j--])
+    {
+      printf("%s is not a palindrome!\n", str);
+      return;
+    }
+  }
+  printf("%s IS a palindrome!\n", str);
 }
