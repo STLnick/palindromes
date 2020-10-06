c2649c70f40e1264f14e57a4e12edbc5de96715f
nicholas ray <nicholas@hoare7.cs.umsl.edu>
2020-10-06 15:47:41 -0500

    :boom: :sparkles: Lots of breaking changes resulting in having a shared mem array accessed in both exe files

--- master.c
+++ master.c
@@ -10,22 +10,27 @@
 
 char** buildstringarray(int *indexcount);
 int detachandremove(int shmid, void *shmaddr);
+void displayhelpinfo();
+
+#define STR_SIZE 20
 
 int main (int argc, char **argv)
 {
-  int childpid; // PID of a child process
-  int id; // ID for the shared memory segment
-  int key = 93; // Key to allocate shared memory segment
+  int childpid;                  // PID of a child process
+  int id;                        // ID for the shared memory array segment
+  key_t key = ftok("/tmp", 'K'); // Key to allocate shared memory segment for array
 
-  int num_proc, num_children, opt, proc_cnt, run_time;
+  /* Values specified by command line arguments - defined below */
+  int num_proc, num_children, opt, run_time;
+
+  int child_cnt = 0;                   // Counter for currently running children processes
   int nflag = 0, sflag = 0, tflag = 0; // Flags for command line options
-  proc_cnt = 0; // Counter for currently running children processes
 
-  int num_strings = 0; // Holds number of strings read in to allocate right amount of shared memory
-  char **strings = buildstringarray(&num_strings); // Holds strings read in to copy into shared memory
-  char **sharedstrings;
+  int num_strings = 0; // Holds number of strings read in
+  char **strings;      // Holds strings read in to copy into shared memory
+  char *sharedstrings; // Array to hold strings in shared memory
 
-  printf("Memory size needed for shared array: %zu\n", sizeof(char *) * num_strings);
+  strings = buildstringarray(&num_strings); // Read in strings to array and store number read in
 
   // Parse command line options
   while ((opt = getopt(argc, argv, "hn:s:t:")) != -1)
@@ -33,23 +38,7 @@ int main (int argc, char **argv)
     switch (opt)
     {
       case 'h': // Help - describe how to run program and default values
-        printf("\nPalindrome master program\n");
-        printf("-------------------------\n");
-        printf("Example command to run ./master:\n\n");
-        printf("./master -n 10 -s 4 -t 60\n\n");
-        printf("-------------------------\n");
-        printf("Program options information:\n");
-        printf("-n = The total number of processes to be ran.\n");
-        printf("-s = The number of children processes to be running at any one time.\n");
-        printf("-t = The time in seconds that the program will execute. This will end\n");
-        printf("     execution even if the processes haven't finished the tasks.\n\n");
-        printf("-------------------------\n");
-        printf("Default value information:\n");
-        printf("All options specified above are optional and will use the\n");
-        printf("following default values if not specified.\n\n");
-        printf("-n = 4\n");
-        printf("-s = 2\n");
-        printf("-t = 100\n\n");
+        displayhelpinfo();
         exit(EXIT_SUCCESS);
         break; 
       case 'n': // Specify number of processes to be ran in total
@@ -70,21 +59,17 @@ int main (int argc, char **argv)
     }
   }
 
+
+
   // Set default values for unused options
   num_proc = nflag ? num_proc : 4;
   num_children = sflag ? num_children : 2;
   run_time = tflag ? run_time : 100;
 
-  printf("num_proc: %d\n", num_proc);
-  printf("num_children: %d\n", num_children);
-  printf("run_time: %d\n", run_time);
 
-  // dummy shared value
-  int *sharedtotal;
 
-  // Allocate shared memory segment
-  // TODO: Change the sizeof() after testing with simple data type
-  if ((id = shmget(key, sizeof(char *) * num_strings, IPC_CREAT | 0660)) == -1)
+  // Allocate shared memory segment for array
+  if ((id = shmget(key, sizeof(char *) * STR_SIZE * num_strings, IPC_CREAT | 0660)) == -1)
   {
     perror("Failed to create shared memory segment.");
     return 1;
@@ -93,32 +78,97 @@ int main (int argc, char **argv)
   // TESTING MESSAGE IF WE SUCCESSFULLY GET SHARED MEMORY
   printf("Successfully allocated shared memory with id: %d\n", id);
 
+
+
   // Attach to the allocated shared memory
-  if ((sharedstrings = (char **) shmat(id, NULL, 0)) == (void *) - 1)
+  if ((sharedstrings = (char *) shmat(id, NULL, 0)) == (void *) - 1)
   {
     perror("Failed to attach shared memory segment.");
     if (shmctl(id, IPC_RMID, NULL) == -1)
       perror("Failed to remove memory segment.");
     return 1;
   }
-
-  // TESTING WITH BASIC DATA TYPE Attach to the allocated shared memory
-  //if ((sharedtotal = (int *) shmat(id, NULL, 0)) == (void *) - 1)
-  //{
-  //  perror("Failed to attach shared memory segment.");
-  //  if (shmctl(id, IPC_RMID, NULL) == -1)
-  //    perror("Failed to remove memory segment.");
-  //  return 1;
-  //}
   printf("Successfully attached to the shared memory segment\n");
 
-  // Detach from and remove the shared memory segment
+
+
+  // TODO: Place strings into sharedstrings for all processes to access
+
+  // Copy strings into shared memory array
+  int i, j;       // For loop counters
+  int srcstrsize; // Holds size of string from 'strings'
+  for (i = 0; i < num_strings; i++)
+  {
+    srcstrsize = strlen(strings[i]);
+    for (j = 0; j < STR_SIZE; j++)
+    {
+      if (strings[i][j] == '\n')
+        sharedstrings[i * STR_SIZE + j] = '\0';
+      else if (j >= srcstrsize)
+        sharedstrings[i * STR_SIZE + j] = '\0';
+      else
+        sharedstrings[i * STR_SIZE + j] = strings[i][j];
+    }  
+  }
+
+  int wpid; // PID of process parent just waited on
+
+  // Max number of time loop will run and in turn the max number of processes that will be ran
+  int maxiterations = num_proc > num_strings ? num_strings : num_proc;
+
+  for (i = 0; i < maxiterations; i++)
+  {
+    // Do stuff
+    if ((childpid = fork()) == -1)
+    {
+      perror("Failed to create child process.");
+      if (detachandremove(id, sharedstrings) == -1)
+        perror("Failed to detach and remove shared memory segment.");
+    }
+
+    child_cnt++; // Add one to currently running children count
+
+    // Child Code
+    if (childpid == 0)
+    {
+      // TODO: - Replace hardcoded index to test, will need to use counter and replace in loop
+
+      char strid[100+1] = {'\0'};
+      sprintf(strid, "%d", id);
+
+      char strrow[100+1] = {'\0'};
+      sprintf(strrow, "%d", i);
+
+      char strmaxsize[100+1] = {'\0'};
+      sprintf(strmaxsize, "%d", STR_SIZE);
+
+      char *args[5] = {"palin", strid, strrow, strmaxsize, '\0'};
+      /* Arguments sent to exec-ed 'palin'
+        * argv[0] = "palin" executable
+        * argv[1] = id of shared memory array
+        * argv[2] = (index of string in shared memory array to test i.e. The 'row' in array to start from)
+        * argv[3] = max size of strings
+      */
+      execv(args[0], args);
+      perror("Child failed to exec command!\n");
+    }
+
+    // Parent code
+    // TODO: - set timer to -t/run_time value, if it runs out kill all children, print message, exit
+ 
+    // If num of children running is at max wait before next loop
+    if (child_cnt == num_children)
+    {
+      wpid = wait(NULL);
+      printf("Parent waited for pid: %i", wpid);
+    }
+  }
+
+
+
+  // Detach from and remove the shared memory array segment
   detachandremove(id, sharedstrings);
-  // TODO: TESTING WITH SIMPLE DATA TYPE: Change second argument after testing
-  //detachandremove(id, sharedtotal);
-
-
-
+  
   return 0;
 }
 
@@ -142,22 +192,43 @@ int detachandremove(int shmid, void *shmaddr)
 
 // Builds and returns string array to copy into shared memory and by reference variable determines number of strings in array
 char** buildstringarray(int *indexcount) {
-  char buf[20]; // Buffer to hold each string read from stdin
+  char buf[20];                            // Buffer to hold each string read from stdin
   char **strings = malloc(sizeof(char *)); // Dynamically built array to hold all strings from stdin
 
-  fgets(buf, 20, stdin); // Get first string from stdin
+  fgets(buf, 20, stdin);             // Get first string from stdin
   strings[*indexcount] = malloc(20); // Use malloc for element - will use realloc in loop
   strcpy(strings[*indexcount], buf); // Copy read string into dynamic array
-  *indexcount += 1; // Increment index counter
+  *indexcount += 1;                  // Increment index counter
 
   // Get rest of strings from input file
   while(fgets(buf, 20, stdin))
   {
     strings = realloc(strings, sizeof(*strings) * (*indexcount + 1)); // Increase size of dynamic array
-    strings[*indexcount] = malloc(20); // Allocate new element's memory space
-    strcpy(strings[*indexcount], buf); // Copy read string into dynamic array
-    *indexcount += 1; // Increment index counter
+    strings[*indexcount] = malloc(20);                                // Allocate new element's memory space
+    strcpy(strings[*indexcount], buf);                                // Copy read string into dynamic array
+    *indexcount += 1;                                                 // Increment index counter
   }
 
   return strings;
+}
+
+void displayhelpinfo()
+{
+  printf("\nPalindrome master program\n");
+  printf("-------------------------\n");
+  printf("Example command to run ./master:\n\n");
+  printf("./master -n 10 -s 4 -t 60\n\n");
+  printf("-------------------------\n");
+  printf("Program options information:\n");
+  printf("-n = The total number of processes to be ran.\n");
+  printf("-s = The number of children processes to be running at any one time.\n");
+  printf("-t = The time in seconds that the program will execute. This will end\n");
+  printf("     execution even if the processes haven't finished the tasks.\n\n");
+  printf("-------------------------\n");
+  printf("Default value information:\n");
+  printf("All options specified above are optional and will use the\n");
+  printf("following default values if not specified.\n\n");
+  printf("-n = 4\n");
+  printf("-s = 2\n");
+  printf("-t = 100\n\n");
 }
--- palin.c
+++ palin.c
@@ -1,14 +1,72 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/shm.h>
 
+int detach(int shmid, void *shmaddr);
 void is_palindrome(char str[]);
 
-int main()
+/*
+* argv[0] = palin
+* argv[1] = id of shared memory segment
+* argv[2] = (index of string in shared memory array to test i.e. the 'row' in array to start)
+* argv[3] = max size of strings
+*/
+
+int main(int argc, char **argv)
 {
-  char str[] = "racecar";
-  char str2[] = "nope";
-  is_palindrome(str);  
-  is_palindrome(str2);
+  // TEST OUTPUT
+  printf("-----------------\n");
+  printf("Hello from palin!\n");
+  printf("-----------------\n");
+  char *sharedstrings;
+
+  int id = atoi(argv[1]);
+  int row = atoi(argv[2]);
+  int strsize = atoi(argv[3]);
+
+  // TODO: Make this file access shared memory to test that string
+  
+  // Attach to the allocated shared memory
+  if ((sharedstrings = (char *) shmat(id, NULL, 0)) == (void *) - 1)
+  {
+    perror("Failed to attach shared memory segment.");
+    if (shmctl(id, IPC_RMID, NULL) == -1)
+      perror("Failed to remove memory segment.");
+    return 1;
+  }
+  printf("From 'palin': Successfully attached to shared memory! id: %i\n", id);
+
+  int i;
+  char buffer[strsize];
+  int nullfound = 0;
+  for (i = 0; i < strsize; i++)
+  {
+    if (!nullfound)
+    {
+      buffer[i] = sharedstrings[row * strsize + i];
+      if (sharedstrings[row * strsize + i] == '\0')
+        nullfound = 1;
+    }
+  }
+
+  printf("TEST: buffer = '%s'", buffer);
+  is_palindrome(buffer);
+  printf("\n");
+
+  
+  // TODO: Develop code to enter the critical section
+  //       Develop the criticalsection() code
+  //         - Write to palin.out or nopalin.out
+  //         - Write to output.log
+  //       Develop code to exit the critical section
+
+
+
+  // detach from shared memory segment
+  detach(id, sharedstrings);
+
   return 0;
 }
 
@@ -17,13 +75,32 @@ void is_palindrome(char str[])
   int i = 0; // Start of string index
   int j = strlen(str) - 1; // End of string index
 
+  // TODO - palindrome ? write string to palin.out : write string to nopalin.out
+  //      - write to logfile => (PID IndexOfString String)
+
   while (j > 1)
   {
     if (str[i++] != str[j--])
     {
-      printf("%s is not a palindrome!\n", str);
+      printf("%s is NOT a palindrome!\n", str);
       return;
     }
   }
   printf("%s IS a palindrome!\n", str);
+}
+
+// Detach from the shared memory segment
+int detach(int shmid, void *shmaddr)
+{
+  int error = 0;
+
+  if (shmdt(shmaddr) == -1)
+    error = errno;
+  if (!error)
+  {
+    printf("From 'palin': Successfully detached the shared memory segment - id: %d\n", shmid);
+    return 0;
+  }
+  errno = error;
+  return -1;
 }
